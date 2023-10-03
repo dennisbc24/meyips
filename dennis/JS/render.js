7fa471aaf307293e0b73cb86e59e0ea309e89a32
dennisbc24 <dennis.bien.vida@gmail.com>
2023-10-03 00:02:26 -0500

    logo

--- dennis/JS/render.js
+++ dennis/JS/render.js
@@ -1,15 +1,6 @@
+
 var botonCargar = document.getElementById('renderBlog');
 var elementoOculto = document.getElementById('tik');
-
-// Funcion para mostrar perfil de tiktok
-botonCargar.addEventListener('click', function() {
-  const cajaGrande = document.getElementById('items');
-  cajaGrande.innerHTML = '';
-  // Muestra el elemento oculto cambiando su estilo
-  elementoOculto.style.display = 'block';
-});
-
-
 
 
 //metodo get desde el frontend
@@ -21,7 +12,6 @@ function traer(eyelash, category) {
   //const urlRaiz = "https://dry-plateau-16443.herokuapp.com";
 
   elementoOculto.style.display = 'none';
-  
 
   const url = `${urlRaiz}/api/v1/${eyelash}`;
   const cajaGrande = document.getElementById('items');
@@ -33,9 +23,6 @@ function traer(eyelash, category) {
       const todosLosElementos = [];
       responseJson.forEach((elemento) => {
         if (elemento.category == category) {
-
-          
-
           const card = document.createElement('div');
           card.className = 'card';
 
@@ -61,7 +48,7 @@ function traer(eyelash, category) {
 
           const mas_info = document.createElement('p');
           mas_info.textContent = 'ver más información';
-          mas_info.setAttribute('_id', elemento._id)
+          mas_info.setAttribute('_id', elemento._id);
 
           const boton_info = document.createElement('button');
           boton_info.textContent = 'Cotizar';
@@ -83,135 +70,111 @@ function traer(eyelash, category) {
     });
 }
 
-traer('products','plantas');
+traer('products', 'plantas');
 
-const eyelashSell = document.getElementById('renderComprar').addEventListener('click', (e) => {
+const eyelashSell = document
+  .getElementById('renderComprar')
+  .addEventListener('click', (e) => {
     //location.reload();
+    var elemento = document.querySelector('.plantasTab');
+    elemento.classList.replace('inactiveTab', 'activeTab');
+
+    var elemento = document.querySelector('.cursosTab');
+    elemento.classList.replace('activeTab', 'inactiveTab');
+
+    var elemento = document.querySelector('.blogTab');
+    elemento.classList.replace('activeTab', 'inactiveTab');
+    
     traer('products', 'plantas');
     //document.getElementById('boton-menu').checked = false;
   });
 
-const eyelashCourses = document.getElementById('renderCursos').addEventListener('click', (e) => {
+const eyelashCourses = document
+  .getElementById('renderCursos')
+  .addEventListener('click', (e) => {
     //location.reload();
+    var elemento = document.querySelector('.plantasTab');
+    elemento.classList.replace('activeTab', 'inactiveTab');
+
+    var elemento = document.querySelector('.cursosTab');
+    elemento.classList.replace('inactiveTab', 'activeTab');
+
+    var elemento = document.querySelector('.blogTab');
+    elemento.classList.replace('activeTab', 'inactiveTab');
     traer('courses', 'plantas');
     //document.getElementById('boton-menu').checked = false;
   });
 
+  // Funcion para mostrar perfil de tiktok
+botonCargar.addEventListener('click', function () {
+  const cajaGrande = document.getElementById('items');
+  cajaGrande.innerHTML = '';
+  // Muestra el elemento oculto cambiando su estilo
+  var elemento = document.querySelector('.plantasTab');
+    elemento.classList.replace('activeTab', 'inactiveTab');
 
+    var elemento = document.querySelector('.cursosTab');
+    elemento.classList.replace('activeTab', 'inactiveTab');
 
-  /* 
-const menuRoperos = document
-  .getElementById('roperosMenu').addEventListener('click', (e) => {
-    //location.reload();
-    traer('roperos');
-    document.getElementById('boton-menu').checked = false;
-  });
-const menuCocina = document
-  .getElementById('cocinaMenu')
-  .addEventListener('click', (e) => {
-    //location.reload();
-    traer('cocina');
-    document.getElementById('boton-menu').checked = false;
-  });
-const menuTermos = document
-  .getElementById('termosMenu')
-  .addEventListener('click', (e) => {
-    //location.reload();
-    traer('termos');
-    document.getElementById('boton-menu').checked = false;
-  });
-const menuEquipaje = document
-  .getElementById('equipajeMenu')
-  .addEventListener('click', (e) => {
-    //location.reload();
-    traer('equipaje');
-    document.getElementById('boton-menu').checked = false;
-  });
-const menuBelleza = document
-  .getElementById('bellezaMenu')
-  .addEventListener('click', (e) => {
-    //location.reload();
-    traer('belleza');
-    document.getElementById('boton-menu').checked = false;
-  });
-
-const botonElectro = document
-  .getElementById('electroCategory')
-  .addEventListener('click', (e) => {
-    traer('electro');
-  });
-
-const botonRoperos = document
-  .getElementById('roperosCategory')
-  .addEventListener('click', (e) => {
-    traer('roperos');
-  });
-
-const botonCocina = document
-  .getElementById('cocinaCategory')
-  .addEventListener('click', (e) => {
-    traer('cocina');
-  });
-
-const botonTermos = document
-  .getElementById('termosCategory')
-  .addEventListener('click', (e) => {
-    traer('termos');
-  });
-
-const botonEquipaje = document
-  .getElementById('equipajeCategory')
-  .addEventListener('click', (e) => {
-    traer('equipaje');
-  });
-
-const botonBelleza = document
-  .getElementById('bellezaCategory')
-  .addEventListener('click', (e) => {
-    traer('belleza');
-  }); */
-
-//function search
-
-function filterSearch(input, selector) {
-  document.addEventListener('keyup', (e) => {
-    if (e.target.matches(input)) {
-      console.log(e.target.value);
-    }
-  });
-}
-
-
-// Obtén referencias a los elementos
-
-
-
-
-
-
-
-var botonMostrar = document.getElementById('mostrarId');
-
-var botonCerrar = document.getElementById('botonCerrar');
-
-// Agrega un controlador de eventos para mostrar el div cuando se presiona el botón
-const boton = document.getElementById('items0').addEventListener('click', e => {
-
-if (e.target.classList.contains('mostrar')) {
-  const getIdMongoElement = e.target.previousElementSibling;
-
-  getIdMongoElement.style.display = 'block';
-}
-
-  
+    var elemento = document.querySelector('.blogTab');
+    elemento.classList.replace('inactiveTab', 'activeTab');
+  elementoOculto.style.display = 'block';
 });
 
-// Agrega un controlador de eventos para cerrar el div cuando se presiona el botón de cerrar
-/* botonCerrar.addEventListener('click', function() {
-  divSobreponible.style.display = 'none';
-}); */
 
 
 
+const eyelashSell2 = document
+  .getElementById('plantasMenu')
+  .addEventListener('click', (e) => {
+    //location.reload();
+    var elemento = document.querySelector('.plantasTab');
+    elemento.classList.replace('inactiveTab', 'activeTab');
 
+    var elemento = document.querySelector('.cursosTab');
+    elemento.classList.replace('activeTab', 'inactiveTab');
+
+    var elemento = document.querySelector('.blogTab');
+    elemento.classList.replace('activeTab', 'inactiveTab');
+    
+    traer('products', 'plantas');
+    document.getElementById('bot-menu').checked = false;
+    //document.getElementById('boton-menu').checked = false;
+  });
+
+const eyelashCourses2 = document
+  .getElementById('cursosMenu')
+  .addEventListener('click', (e) => {
+    //location.reload();
+    var elemento = document.querySelector('.plantasTab');
+    elemento.classList.replace('activeTab', 'inactiveTab');
+
+    var elemento = document.querySelector('.cursosTab');
+    elemento.classList.replace('inactiveTab', 'activeTab');
+
+    var elemento = document.querySelector('.blogTab');
+    elemento.classList.replace('activeTab', 'inactiveTab');
+    traer('courses', 'plantas');
+    document.getElementById('bot-menu').checked = false;
+    //document.getElementById('boton-menu').checked = false;
+  });
+
+  // Funcion para mostrar perfil de tiktok
+  const eyelashCourses3 = document
+  .getElementById('socialMenu')
+  .addEventListener('click', function () {
+  const cajaGrande = document.getElementById('items');
+  cajaGrande.innerHTML = '';
+  // Muestra el elemento oculto cambiando su estilo
+  var elemento = document.querySelector('.plantasTab');
+    elemento.classList.replace('activeTab', 'inactiveTab');
+
+    var elemento = document.querySelector('.cursosTab');
+    elemento.classList.replace('activeTab', 'inactiveTab');
+
+    var elemento = document.querySelector('.blogTab');
+    elemento.classList.replace('inactiveTab', 'activeTab');
+  elementoOculto.style.display = 'block';
+  document.getElementById('bot-menu').checked = false;
+});
 
